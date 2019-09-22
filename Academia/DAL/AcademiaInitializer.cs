8b5aabf6894b3898945b9e1aaac9d55435f80492
lucas randisi <lucasrandisi@gmail.com>
2019-09-22 09:31:26 -0300

    Agregada una persona en el initializer

--- Academia/DAL/AcademiaInitializer.cs
+++ Academia/DAL/AcademiaInitializer.cs
@@ -10,20 +10,20 @@ namespace Academia.DAL {
     public class AcademiaInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<AcademiaContext> {
         protected override void Seed(AcademiaContext context) { 
             var personas = new List<Persona> {
-                new Persona { Nombre = "Lucas", Apellido = "Randisi", Direccion = "Valle 1234", Email = "lucasrandisi@gmail.com", Telefono = "34144444", FechaNacimiento = DateTime.ParseExact("1997-09-10", "yyyy-MM-dd",System.Globalization.CultureInfo.InvariantCulture), Legajo = 4444 } 
+                new Persona { Nombre = "Lucas", Apellido = "Randisi", Direccion = "Valle 1234", Email = "lucasrandisi@gmail.com", Telefono = "34144444", FechaNacimiento = DateTime.ParseExact("1997-09-10", "yyyy-MM-dd",System.Globalization.CultureInfo.InvariantCulture), Legajo = 4444, TipoPersona = Persona.Rol.Alumno } 
             };
 
             personas.ForEach(p => context.Personas.Add(p));
             context.SaveChanges();
 
-            //var usuarios = new List<Usuario> {            
-            //    new Usuario {NombreUsuario = "lucas144", Clave = "1234", Habilitado = true, CambioClave = false, PersonaID = 1},
-            //    new Usuario {NombreUsuario = "agusdelm", Clave = "elchicorubio", Habilitado = true, CambioClave = false, PersonaID = 1},
-            //    new Usuario {NombreUsuario = "tinchopereyra", Clave = "central", Habilitado = true, CambioClave = false, PersonaID = 1},
-            //};
+            var usuarios = new List<Usuario> {
+                new Usuario {NombreUsuario = "lucas144", Clave = "1234", Habilitado = true, CambioClave = false, PersonaID = 1},
+                new Usuario {NombreUsuario = "agusdelm", Clave = "elchicorubio", Habilitado = true, CambioClave = false, PersonaID = 1},
+                new Usuario {NombreUsuario = "tinchopereyra", Clave = "central", Habilitado = true, CambioClave = false, PersonaID = 1},
+            };
 
-            //usuarios.ForEach(u => context.Usuarios.Add(u));
-            //context.SaveChanges();
+            usuarios.ForEach(u => context.Usuarios.Add(u));
+            context.SaveChanges();
         }
     }
 }
