53e8b1c849e86083dc4992e91e708f1fc286a655
Agustin <agus.delmonti@gmail.com>
2019-10-27 18:45:37 -0300

    Repositorio: Ejemplo simple GetAll

---
+++ Data/Persistance/CursoRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data.Persistence;
+using Entities;
+
+namespace Data.Repositories
+{
+    public class CursoRepository : Repository<Curso>, ICursoRepository
+    {
+        public CursoRepository(AcademiaContext context) : base(context) { }
+
+        public AcademiaContext CursoContext { get; set; }
+
+        public IEnumerable<Curso> GetAll()
+        {
+            return CursoContext.Cursos.ToList();
+        }
+    }
+}
---
+++ Data/Repositories/ICursoRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using Entities;
+
+namespace Data.Repositories
+{
+    interface ICursoRepository : IRepository<Curso>
+    {
+        IEnumerable<Curso> GetAll();
+    }
+}
