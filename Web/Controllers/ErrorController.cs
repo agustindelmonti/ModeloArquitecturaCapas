3b00294eba9793ca6188a0db073d8fbb2b26cd68
Agustin <agus.delmonti@gmail.com>
2019-11-01 17:32:57 -0300

    Routing de errores web
    Rediseño de pagina 404

---
+++ Web/Controllers/ErrorController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    [Authorize]
+    public class ErrorController : Controller
+    {
+
+        public ActionResult Oops(int id)
+        {
+            Response.StatusCode = id;
+            return View();
+        }
+    }
+}
