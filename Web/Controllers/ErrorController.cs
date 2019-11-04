ea4a16cf3caaabec009c0373f445d8130dc6221b
Agustin <agus.delmonti@gmail.com>
2019-11-04 14:11:35 -0300

    Nav bar por Usuarios

--- Web/Controllers/ErrorController.cs
+++ Web/Controllers/ErrorController.cs
@@ -1,16 +1,17 @@
-﻿using System;
+﻿using BusinessLogic.Authorization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace Web.Controllers
 {
 
     public class ErrorController : Controller
     {
-
         public ActionResult Oops(int id)
         {
             Response.StatusCode = id;
