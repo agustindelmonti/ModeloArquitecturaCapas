d6db7b1d38aab0e4f7ef1ff99933e0195617a250
Agustin <agus.delmonti@gmail.com>
2019-11-09 14:52:09 -0300

    Diseño vista profesor

--- Web/Controllers/DocenteController.cs
+++ Web/Controllers/DocenteController.cs
@@ -22,26 +22,26 @@ namespace Web.Controllers
             return View();
         }
 
-        public ActionResult MisCursos(string id, bool editar = false) {
+        // GET: MisCursos
+        public ActionResult MisCursos() {
 
             int userID = Convert.ToInt32(HttpContext.User.Identity.Name);
-
-            ViewBag.EditMode = editar;
-
             Persona persona = usuarioLogic.GetPersonaByUserID(userID);
 
-            if (String.IsNullOrEmpty(id)) {
-                IEnumerable<Curso> cursosDocente = cursoLogic.FindCursosActualesDocenteByPersonaID(persona.PersonaID);
+            IEnumerable<Curso> cursosDocente = cursoLogic.FindCursosActualesDocenteByPersonaID(persona.PersonaID);
 
-                return View(cursosDocente);
-            }
-            else {
-                int cursoID = Convert.ToInt32(id);
-                int docenteID = persona.PersonaID;
-                alumnosCurso = inscripcionLogic.FindInscripcionesByCursoIDAndPersonaID(cursoID, docenteID);
+            return View(cursosDocente);
+        }
 
-                return View("AlumnosCurso", alumnosCurso);
-            }
+        public ActionResult AlumnosCurso(string id, bool editar = false)
+        {
+            int docenteID = Convert.ToInt32(HttpContext.User.Identity.Name);
+            int cursoID = Convert.ToInt32(id);
+
+            alumnosCurso = inscripcionLogic.FindInscripcionesByCursoIDAndPersonaID(cursoID, docenteID);
+            ViewBag.EditMode = editar;
+
+            return View("AlumnosCurso", alumnosCurso);
         }
 
 
